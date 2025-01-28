0dba8d8701e3a3578b089b322abc6030618c431e
vanda-mugo <johnmugo006@gmail.com>
2025-01-28 03:19:12 +0300

    Added pixel transition for the services section

--- vanda-portfolio/src/components/Services/Services.tsx
+++ vanda-portfolio/src/components/Services/Services.tsx
@@ -1,5 +1,17 @@
 import React from "react";
 import './Services.css';
+import PixelTransition from '../PixelTransition/PixelTransition';
+import PixelCard from '../PixelCard/PixelCard'; 
+import c2 from '../../assets/ServicesBackgrounds/c2.jpg';
+import c22 from '../../assets/ServicesBackgrounds/c22.jpg';
+import c3 from '../../assets/ServicesBackgrounds/c3.jpg';
+import c33 from '../../assets/ServicesBackgrounds/c33.jpg';
+import c4 from '../../assets/ServicesBackgrounds/c4.jpg';
+import c44 from '../../assets/ServicesBackgrounds/c44.jpg';
+import c5 from '../../assets/ServicesBackgrounds/c5.jpg';
+import cc5 from '../../assets/ServicesBackgrounds/cc5.jpg';
+
+
 
 export const Services = () => {
     const [selectedService, setSelectedService] = React.useState("FrontEnd-Development");
@@ -10,6 +22,7 @@ export const Services = () => {
 
     return (
         <div className="services wrapper">
+            <div className="headerLine">Services</div>
             <div className="container">
                 <input
                     type="radio"
@@ -20,13 +33,40 @@ export const Services = () => {
                     onChange={handleServiceChange}
                 />
                 <label htmlFor="c1" className="card">
-                    <div className="row">
-                        <div className="description">
-                            <h4>FrontEnd Engineering</h4>
-                            <p>Details about FrontEnd Engineering services...</p>
-                        </div>
-                        <div className="icon"></div>
-                    </div>
+                {selectedService === "FrontEnd-Development" ? (
+                            <PixelTransition
+                            firstContent={
+                            <img
+                                src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F91%2F9b%2F79%2F919b7945c8de403223d06a1936ab5d2a.jpg&f=1&nofb=1&ipt=664661ad07743cc5fd8ff44b298d0ca0ebb70fada12429a1c6e7f2b67f6c9b44&ipo=images"
+                                alt="default pixel transition content, a cat!"
+                                style={{ width: "100%", height: "100%", objectFit: "cover" }}
+                            />
+
+                            }
+                            secondContent={
+                            <div
+                                style={{
+                                width: "100%",
+                                height: "100%",
+                                display: "grid",
+                                placeItems: "center",
+                                backgroundColor: "#111"
+                                }}
+                            >
+                                <img
+                                src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmiro.medium.com%2Fmax%2F3200%2F1*kstvj_yU58Mqyedi7sqY8Q.png&f=1&nofb=1&ipt=68ee2c1007014637f94bb427e2ada19206758ad8812d8290018008822889009f&ipo=images"
+                                alt="default pixel transition content, a cat!"
+                                style={{ width: "100%", height: "100%", objectFit: "cover" }}
+                            />
+                            </div>
+                            
+                            }
+                            gridSize={12}
+                            pixelColor='cyan'
+                            animationStepDuration={0.6}
+                            className="custom-pixel-card"
+                        /> 
+                        ): (<div className="icon"></div>)}
                 </label>
 
                 <input
@@ -38,13 +78,40 @@ export const Services = () => {
                     onChange={handleServiceChange}
                 />
                 <label htmlFor="c2" className="card">
-                    <div className="row">
-                        <div className="description">
-                            <h4>Graphic Design</h4>
-                            <p>Details about Graphic Design services...</p>
-                        </div>
-                        <div className="icon"></div>
-                    </div>
+                {selectedService === "graphicDesign" ? (
+                        <PixelTransition
+                            firstContent={
+                            <img
+                                src={c2}
+                                alt="default pixel transition content, a cat!"
+                                style={{ width: "100%", height: "100%", objectFit: "cover" }}
+                            />
+
+                            }
+                            secondContent={
+                            <div
+                                style={{
+                                width: "100%",
+                                height: "100%",
+                                display: "grid",
+                                placeItems: "center",
+                                backgroundColor: "#111"
+                                }}
+                            >
+                                <img
+                                src={c22}
+                                alt="default pixel transition content, a cat!"
+                                style={{ width: "100%", height: "100%", objectFit: "cover" }}
+                            />
+                            </div>
+                            
+                            }
+                            gridSize={10}
+                            pixelColor='cyan'
+                            animationStepDuration={0.4}
+                            className="custom-pixel-card"
+                        /> 
+                        ): (<div className="icon"></div>)}
                 </label>
 
                 <input
@@ -56,13 +123,40 @@ export const Services = () => {
                     onChange={handleServiceChange}
                 />
                 <label htmlFor="c3" className="card">
-                    <div className="row">
-                        <div className="description">
-                            <h4>BackEnd Engineering</h4>
-                            <p>Details about BackEnd Engineering services...</p>
-                        </div>
-                        <div className="icon"></div>
-                    </div>
+                {selectedService === "BackEnd-Development" ? (
+                        <PixelTransition
+                            firstContent={
+                            <img
+                                src={c3}
+                                alt="default pixel transition content, a cat!"
+                                style={{ width: "100%", height: "100%", objectFit: "cover" }}
+                            />
+
+                            }
+                            secondContent={
+                            <div
+                                style={{
+                                width: "100%",
+                                height: "100%",
+                                display: "grid",
+                                placeItems: "center",
+                                backgroundColor: "#111"
+                                }}
+                            >
+                                <img
+                                src={c33}
+                                alt="default pixel transition content, a cat!"
+                                style={{ width: "100%", height: "100%", objectFit: "cover" }}
+                            />
+                            </div>
+                            
+                            }
+                            gridSize={12}
+                            pixelColor='cyan'
+                            animationStepDuration={0.4}
+                            className="custom-pixel-card"
+                        /> 
+                        ): (<div className="icon"></div>)}
                 </label>
 
                 <input
@@ -74,13 +168,40 @@ export const Services = () => {
                     onChange={handleServiceChange}
                 />
                 <label htmlFor="c4" className="card">
-                    <div className="row">
-                        <div className="description">
-                            <h4>Flat Art Design</h4>
-                            <p>Details about Flat-Art-Design services...</p>
-                        </div>
-                        <div className="icon"></div>
-                    </div>
+                {selectedService === "Flat-Art-Design" ? (
+                        <PixelTransition
+                            firstContent={
+                            <img
+                                src={c4}
+                                alt="default pixel transition content, a cat!"
+                                style={{ width: "100%", height: "100%", objectFit: "cover" }}
+                            />
+
+                            }
+                            secondContent={
+                            <div
+                                style={{
+                                width: "100%",
+                                height: "100%",
+                                display: "grid",
+                                placeItems: "center",
+                                backgroundColor: "#111"
+                                }}
+                            >
+                                <img
+                                src={c44}
+                                alt="default pixel transition content, a cat!"
+                                style={{ width: "100%", height: "100%", objectFit: "cover" }}
+                            />
+                            </div>
+                            
+                            }
+                            gridSize={12}
+                            pixelColor='cyan'
+                            animationStepDuration={0.4}
+                            className="custom-pixel-card"
+                        /> 
+                        ): (<div className="icon"></div>)}
                 </label>
 
                 <input
@@ -92,13 +213,40 @@ export const Services = () => {
                     onChange={handleServiceChange}
                 />
                 <label htmlFor="c5" className="card">
-                    <div className="row">
-                        <div className="description">
-                            <h4>Drone Videography</h4>
-                            <p>Details about Drone Videography services...</p>
-                        </div>
-                        <div className="icon"></div>
-                    </div>
+                {selectedService === "Drone-Videography" ? (
+                        <PixelTransition
+                            firstContent={
+                            <img
+                                src={c5}
+                                alt="default pixel transition content, a cat!"
+                                style={{ width: "100%", height: "100%", objectFit: "cover" }}
+                            />
+
+                            }
+                            secondContent={
+                            <div
+                                style={{
+                                width: "100%",
+                                height: "100%",
+                                display: "grid",
+                                placeItems: "center",
+                                backgroundColor: "#111"
+                                }}
+                            >
+                                <img
+                                src={cc5}
+                                alt="default pixel transition content, a cat!"
+                                style={{ width: "100%", height: "100%", objectFit: "cover" }}
+                            />
+                            </div>
+                            
+                            }
+                            gridSize={12}
+                            pixelColor='rgba(0,255,255,1)'
+                            animationStepDuration={0.4}
+                            className="custom-pixel-card"
+                        /> 
+                        ): (<div className="icon"></div>)}
                 </label>
             </div>
         </div>
